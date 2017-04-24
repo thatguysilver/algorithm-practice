4b32c1a6d372c58a351c8dd76745ed7375cc46e8
Adam <adampaulsilver@gmail.com>
2017-04-23 22:15:50 -0400

    py string reverser

---
+++ py/reverse_string.py
@@ -0,0 +1,14 @@
+'''Gonna work on a very basic prompt from freecodeacademy.com,
+a string reverser. Py is way easier than js to work with, so
+I'm not expecting this to be really hard to do at all.'''
+
+def reverse(word):
+    for i in range(len(word)):
+        return str(word[::-1])
+'''typical python list slicing notation is going to be start, end, increment.'''
+
+print(reverse("Potato"))
+
+'''
+Can't wait to spend four hours doing the same thing in js!! :'|
+'''
