99bd0024979bfdf9d62bca59f8df37005b0c901a
Adam Silver <adampaulsilver@gmail.com>
2017-04-26 00:51:37 -0400

    Finished palindrome. Surprisingly, it's easier in js.

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,18 +1,20 @@
+palindrome("My age is 0, 0 si ega ym.");
 
 function palindrome(word) {
   // Good luck!
+  a = true;
   lower = word.toLowerCase();
-  newWord = lower.replace(/_|\*|,|\./gi, ''); //g is global, i is ignore; these flags in conjunction allow to match
+  newWord = lower.replace(/ |_|,|\.|-|\(|\)|:|\/|\//gi, ''); //g is global, i is ignore; these flags in conjunction allow to match
 
   arrayWord = newWord.split(''); //creates an array from the string;
+  length = Math.floor((arrayWord.length)/2);
+ // return length;
+  for (var i=0; i <= (length); i++) {
+    a = arrayWord[i] === arrayWord[arrayWord.length-1-i];
+    if (a===false) {break;} else {continue;}
 
-  for (var i=0; i <= (arrayWord.length); i++) {
-    return arrayWord[i] == arrayWord[arrayWord.length-1-i];
   }
-
+  return a;
   //return arrayOfStrings; //for testing purposes
 }
-
-
-
-palindrome("My age is 0, 0 si ega ym.");
+palindrome("eye");
