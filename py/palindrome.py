f419634609c1664aebfc190b3db8a909d2a60ad4
Adam <adampaulsilver@gmail.com>
2017-04-25 22:22:03 -0400

    ...

--- py/palindrome.py
+++ py/palindrome.py
@@ -1,8 +1,13 @@
 #Gotta make a palindrome tester. It must return true
 #if it's palindromic. It must have lowercase/uppercase whatever.
+import re
 
-def is_palindrome(word):
+def is_palindrome(raw_word):
 
+    word = re.match(r'[^a-zA-Z]+', raw_word)
+    if word.group() != None: 
+        print(word.group())
+    '''
     for i in range(0, len(word)):
         a = (word[i].lower() == word[-(i+1)].lower())
 
@@ -14,8 +19,10 @@ def is_palindrome(word):
             break
     if a == True:
         print("It is a palindrome.")
+'''
 
-is_palindrome("abcdefgFedcba")
+is_palindrome("abcdefgfedcba")
+is_palindrome("a!#bcd#ef@gFed%cba")
 
 '''
 This is a work in progress; I need to improve my regex skills if I
