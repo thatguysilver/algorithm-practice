5c8b8d7683f4b8d651c5b62be8248669149461c7
Adam Silver <adampaulsilver@gmail.com>
2017-04-28 21:38:13 -0400

    py longestword

--- py/longestword.py
+++ py/longestword.py
@@ -8,15 +8,14 @@ def longest(string):
     #print(biggest_word)
     
     for i in range(1, len(a)):
-        #print(a[i])
-        if biggest_word < a[i]:
+        if len(biggest_word) <len(a[i]):
             biggest_word = a[i]
+            
     
-    #print(biggest_word) 
+    print(biggest_word) 
     
     #same problem as with the js version of this problem; It's returning
     #the final item in the string.
 
 
-
-longest("The quick brown fox jumped over the thing")
+longest("This is a sentence with one jakf;djsakljfkdl;s word")
